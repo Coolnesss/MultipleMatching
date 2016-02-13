4bd9574247590ebf55e282638b8be8618de08f1f
Chang <chang.rajani@gmail.com>
2016-02-13 17:48:13 +0200

    Performance tests allow use of varargs for config

--- performance.cpp
+++ performance.cpp
@@ -8,8 +8,8 @@ using namespace chrono;
 string text;
 vector<string> random_patterns;
 
-const ll PATTERN_LENGTH_MAX = 500;
-const ll PATTERN_AMOUNT = 2000;
+ll PATTERN_LENGTH_MAX = 500;
+ll PATTERN_AMOUNT = 2000;
 
 void load_text(string path) {
 	ifstream in(path);
@@ -30,11 +30,22 @@ vector<pair<ll,ll>> run_ac_performance_tests() {
   return findPattern(text);
 }
 
-int main(int argc, char* ARGV[]) {
-  string asd = string(ARGV[1]);
-  load_text(asd);
-  load_random_patterns();
+vector<pair<ll,ll>> run_kr_performance_tests() {
+  return findPatterns(text, random_patterns);
+}
 
+// Argument 1 contains path to file,
+// 2 should contain amount of patterns and 3 length of patterns
+void load_args_and_data(int argc, char* argv[]) {
+		if (argc >= 3) PATTERN_AMOUNT = (ll) argv[2];
+		if (argc >= 4) PATTERN_LENGTH_MAX = (ll) argv[3];
+	  string path = string(argv[1]);
+	  load_text(path);
+	  load_random_patterns();
+}
+
+int main(int argc, char* argv[]) {
+	load_args_and_data(argc, argv);
   auto start = steady_clock::now();
   preprocess_ac(random_patterns);
   auto end = steady_clock::now();
@@ -43,6 +54,18 @@ int main(int argc, char* ARGV[]) {
 
   start = steady_clock::now();
   run_ac_performance_tests();
+  end = steady_clock::now();
+
+  cout <<  duration_cast<milliseconds>(end - start).count() << endl;
+
+  start = steady_clock::now();
+  preprocess_KR(random_patterns, text);
+  end = steady_clock::now();
+
+  cout <<  duration_cast<milliseconds>(end - start).count() << endl;
+
+  start = steady_clock::now();
+  run_kr_performance_tests();
   end = steady_clock::now();
 
   cout <<  duration_cast<milliseconds>(end - start).count() << endl;
--- run_tests.rb
+++ run_tests.rb
@@ -1,5 +1,7 @@
+$test_file = "english.50MB"
+
 def run_correctness_tests
-  compile_command = "g++ -std=c++11 -Ofast test.cpp ahocorasick.cpp karprabin.cpp"
+  compile_command = "clang++ -std=c++11 -Ofast test.cpp ahocorasick.cpp karprabin.cpp"
   system(compile_command)
   output = `./a.out`
 
@@ -16,8 +18,42 @@ def run_correctness_tests
   else
     puts "Karp-Rabin correctness tests completed successfully."
   end
+end
 
+def print_output
+  prep, search = `./a.out #{$test_file}`.split("\n")
+  puts "Preprocessing: #{prep} ms"
+  puts "Search: #{search} ms"
+end
 
+def run_performance_tests
+  puts "Running Aho-Corasick without fprofile..."
+  compile_command = "g++ -std=c++11 -Ofast performance.cpp ahocorasick.cpp karprabin.cpp"
+  system(compile_command)
+  print_output
+
+  puts "Running Aho-Corasick with fprofile-generate..."
+  system("g++ -std=c++11 -fprofile-generate -Ofast performance.cpp ahocorasick.cpp karprabin.cpp")
+  `./a.out #{$test_file}`
+
+  puts "Compiling with fprofile-use..."
+  system("g++ -std=c++11 -fprofile-use -Ofast performance.cpp ahocorasick.cpp karprabin.cpp")
+  puts "Running actual program"
+  print_output
+
+  puts "Running Karp-Rabin without fprofile..."
+  compile_command = "g++ -std=c++11 -Ofast performance.cpp ahocorasick.cpp karprabin.cpp"
+  system(compile_command)
+  print_output
+
+  puts "Running Karp-Rabin with fprofile-generate..."
+  system("g++ -std=c++11 -fprofile-generate -Ofast performance.cpp ahocorasick.cpp karprabin.cpp")
+  `./a.out #{$test_file}`
+
+  puts "Compiling with fprofile-use..."
+  system("g++ -std=c++11 -fprofile-use -Ofast performance.cpp ahocorasick.cpp karprabin.cpp")
+  puts "Running actual program"
+  print_output
 end
 
 run_correctness_tests if ARGV.include? "c"
